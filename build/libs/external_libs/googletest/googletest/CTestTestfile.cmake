a84984cde422f1830ac8981ccf0712769dd5d971
AhmadSaeedZaidi <ahmadsaeedzaidi@gmail.com>
2025-06-11 15:36:35 +0500

    Delete build directory
    oopsie, forgot to setup a gitignore

--- build/libs/external_libs/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Zaidi/OneDrive - Higher Education Commission/Documents/course books/internship-arc/opengl-game/libs/external_libs/googletest/googletest
-# Build directory: C:/Users/Zaidi/OneDrive - Higher Education Commission/Documents/course books/internship-arc/opengl-game/build/libs/external_libs/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/libs/internal_libs/mylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Zaidi/OneDrive - Higher Education Commission/Documents/course books/internship-arc/opengl-game/libs/internal_libs/mylib
-# Build directory: C:/Users/Zaidi/OneDrive - Higher Education Commission/Documents/course books/internship-arc/opengl-game/build/libs/internal_libs/mylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
